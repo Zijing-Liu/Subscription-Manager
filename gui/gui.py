186ce9a03f5aacc68e99aac9104a4850057b2098
Celine Liu <zijingliu2021@outlook.com>
2023-06-18 17:54:15 -0700

    handled the password encryption and  login verification on the backend,

--- gui/gui.py
+++ gui/gui.py
@@ -29,8 +29,7 @@ def signUpReq():
     data = {
         'name': name_text,
         'email': email_text,
-        'password_hashed': hashed_password_str_g,
-        'salt': salt_g,
+        'password_plain': password_text,
     }
 
     try:
@@ -41,42 +40,22 @@ def signUpReq():
 
         
 # Make a get request method to the login endpoint, pass the email_text1 as param
-def lognInReq(email_text1):
-    print("email is" + email_text1)
+def logInReq():
     url = 'http://localhost:8000/login' 
-    params = {'email': email_text1}  # Pass the email as a parameter
-    print(params["email"])
-    response = requests.get(url, params = params)
+    data = {'email': email_text1,
+            'password': password_text1
+            }  
 
-    if response.status_code == 200:
-        print('printing the password stroed in the database' + response)
-        return response.text  # hashed password string
-    else:
-        print('Request failed with status code:', response.status_code)
+    response = requests.post(url)
+    try:
+        response = requests.post(url, json=data)
+        response.raise_for_status()  # Check for any errors
+        return response
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
 ################################################# HTTP requests end here
 
-#### helper funciton ####
-    # encryptPassword using bcrypt and return a hash value and a salt 
-def encryptPassword(password_text):
-    # converting password to array of bytes
-    bytes = password_text.encode('utf-8')
-    # generating the salt
-    salt = bcrypt.gensalt()
-    # Hashing the password
-    password_text_hashed = bcrypt.hashpw(bytes, salt)
-    # convert the passwrod from byte to string
-    hashed_password_str = password_text_hashed.decode('utf-8')
-    salt_str = salt.decode('utf-8')
 
-    encryption = {
-    'salt': salt_str,
-    'password_hashed': hashed_password_str
-    }
-    print(" The type of salt is: ")
-    print(type(encryption["salt"]))
-    print(type(encryption["password_hashed"]))
-    return encryption
-#### end of helper funciton ###
 
 
 
@@ -168,8 +147,8 @@ def sign_up():
 def register():
     global name_text
     global email_text
-    global hashed_password_str_g
-    global salt_g
+    global password_text
+
 
     # TESTING!!!
     # Set registered as a boolean operator to False
@@ -185,7 +164,7 @@ def register():
     curr_users_data = getData()
     existing_emails = []
     for row in curr_users_data:
-        existing_emails.append(row[2])
+        existing_emails.append(row[0])
     if (email_text in existing_emails):
         registered = True
 
@@ -235,11 +214,6 @@ def register():
         try_again_btn.pack(pady=20)
     # If not registered, write user info to the file
     else:
-        # call encryptPassword function to encode the password_text user registered with, and store salt and hashed_password_str variables
-        encryption =  encryptPassword(password_text)
-        salt_g = encryption["salt"]
-        hashed_password_str_g =  encryption["password_hashed"]
-
         # send the post request to the backend server
         signUpReq()
         # Clear out the entry box
@@ -317,33 +291,13 @@ def login_verify():
     password_text1 = password_verify.get()
     print(email_text1)
 
-    # TESTING!!!
-    # Loop through each line in the file, and check if the email and password entered match the records in the database
     # Set login as False by default
     login = False
-    # for line in open('gui/credentials.txt', 'r').readlines():
-    #     login_info = line.split()
-    #     # If the email and password entered match the records in the database, set login to True
-    #     if email_text1 == login_info[3] and password_text1 == login_info[5]:
-    #         login = True
-    #         break
 
-    # get the sotre password_hashed_str from the database by calling the get method
-    password_hashed_str = lognInReq(email_text1)
-    print("!!!")
-    print(password_hashed_str)
-    # conver the str back to byte
-    hashed_password_bytes = password_hashed_str.encode('utf-8')
-    # encode the password that user input]
-    password_text1_bytes= password_text1.encode('utf-8')
-
-    # If the email and password entered match the records in the database, set login to True
-    if password_text1_bytes == hashed_password_bytes:
-        login = True
-
-
-
-    # When the email and password are correct
+    # if the password verification is successcual , recieve response result (true) from the backend and assign it to the login variable
+    login = logInReq()
+    
+    # if login is set to ture, open the homepage
     if login:
         homepage()
     # When the email and/or password are incorrect
--- testApp/main.py
+++ testApp/main.py
@@ -3,11 +3,13 @@ from flask import Flask, request, redirect
 import tkinter as tk
 from flask_restful import Api, Resource
 # from flask_sqlalchemy import SQLAlchemy
-import sqlite3
+import sqlite3, bcrypt
 
 # initialize this flask application, api
 app = Flask(__name__)
 api = Api(app)
+
+#### helper funciton ####
 
 # define a function to connect to the sql database
 def get_db_connection():
@@ -16,6 +18,26 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+    # encryptPassword using bcrypt and return a hash value and a salt 
+def encryptPassword(password_text):
+    # converting password to array of bytes
+    bytes = password_text.encode('utf-8')
+    # generating the salt
+    salt = bcrypt.gensalt()
+    # Hashing the password
+    password_text_hashed = bcrypt.hashpw(bytes, salt)
+    # convert the passwrod from byte to string
+    hashed_password_str = password_text_hashed.decode('utf-8')
+    salt_str = salt.decode('utf-8')
+
+    encryption = {
+    'salt': salt_str,
+    'password_hashed': hashed_password_str
+    }
+
+    return encryption
+
+#### end of helper funciton ###
 # Define a resource class for the user Signup endpoint
 class UserSignUp(Resource):
     # define the get method that fetch all the user data from the data.db database and return the data as a list of tuple named users_list
@@ -23,9 +45,10 @@ class UserSignUp(Resource):
         try:
             users_list = []
             conn = get_db_connection()
-            for row in conn.execute('SELECT * FROM User').fetchall():
+            for row in conn.execute('SELECT email FROM User').fetchall():
                 users_list.append(row)
             users_list = [tuple(row) for row in users_list]
+            print(users_list)
             conn.commit()
             conn.close()
             return users_list
@@ -37,13 +60,14 @@ class UserSignUp(Resource):
         usr_data = request.get_json()
         name = usr_data.get('name')
         email = usr_data.get('email')
-        password = usr_data.get('password_hashed')
-        salt = usr_data.get('salt')
+        password = usr_data.get('password_plain')
+        salt = encryptPassword(password)['salt']
+        password_hashed = encryptPassword(password)['password_hashed']
         try:
             conn = get_db_connection()
             conn.execute('''INSERT OR REPLACE INTO User
                 (name, email, password_hashed, salt) VALUES (?, ?, ?, ?)''',
-                         (name, email, password, salt))
+                         (name, email, password_hashed, salt))
             
             conn.commit()
             conn.close()
@@ -55,16 +79,24 @@ class UserSignUp(Resource):
 
 # Define a UserLogin resource for the 'login' endpoint
 class UserLogIn(Resource):
-    def get(self, params):
-        email = params["email"]
+    def post(self):
+        login_data = request.get_json()
+        user_email = login_data.get("email")
+        user_password = login_data.get("password")
         conn = get_db_connection()
+        
         try:
-            hash = conn.execute('SELECT password_hashed, salt FROM USER WHERE email = ?', (email,)).fetchone()
+            hash = conn.execute('SELECT password_hashed, salt FROM USER WHERE email = ?', (user_email,)).fetchone()
             if hash is None:
                 return "Email not found" 
             password_hashed = hash[0]
-            salt = hash[1]
-            return (password_hashed, salt)
+
+            # conver the str back to byte
+            hashed_password_bytes = password_hashed.encode('utf-8')
+            # encode the password that user input]
+            user_password_bytes= user_password.encode('utf-8')
+            return user_password_bytes == hashed_password_bytes
+                
         
         except Exception as e:
             print("An error occurred:", str(e))
